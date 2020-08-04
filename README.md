149e763952158d7ae3a77b1cb38e9cf817e77eb2
archena <archena@users.noreply.github.com>
2020-08-04 21:00:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Really quick start with the Arduino Nano 33 BLE and PlatformIO
+
+## Step 1: mount Arduino on breadboard
+
+See the attached image
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
